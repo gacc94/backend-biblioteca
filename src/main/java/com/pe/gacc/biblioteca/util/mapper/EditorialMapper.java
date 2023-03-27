8cc48a28974f72b87525b30ce0deccf52af83d7d
Gustavo Caqui <gustavocaqui94@gmail.com>
2023-03-26 20:58:58 -0500

    implementacion de dependencia maptruct e interface EditorialMapper

---
+++ src/main/java/com/pe/gacc/biblioteca/util/mapper/EditorialMapper.java
@@ -0,0 +1,11 @@
+package com.pe.gacc.biblioteca.util.mapper;
+
+import com.pe.gacc.biblioteca.domain.dto.EditorialDTO;
+import com.pe.gacc.biblioteca.entity.Editorial;
+import org.mapstruct.Builder;
+import org.mapstruct.Mapper;
+
+@Mapper(builder = @Builder(disableBuilder = true))
+public interface EditorialMapper {
+    public EditorialDTO toDTO(Editorial editorial);
+}
